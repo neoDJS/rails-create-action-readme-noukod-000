d5f42cfa3c804f51eced11ff825fe2436ef3aa1c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-02-20 15:34:04 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,13 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def create
+		post = Post.new
+		post.title = params[:title]
+		post.description = params[:description]
+		post.save
+	end
+
 	def new
 		@post = Post.new
 	end
